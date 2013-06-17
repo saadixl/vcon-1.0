90032f13784d4fe545d34eeeaaa2fc8f6ba40709
saadisky <saadixl@gmail.com>
2013-06-17 18:12:15 +0600

    This is a Windows voice controller

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace vCon_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "You can now speak and command your PC to do as you say.";
+            this.notifyIcon1.BalloonTipTitle = "vCon 1.0 is running now.";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "vCon 1.0";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(156, 22);
+            this.toolStripMenuItem1.Text = "About vCon 1.0";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(156, 22);
+            this.toolStripMenuItem2.Text = "Exit";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(116, 0);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(196, 38);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "vCon 1.0";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
+using System.Speech.Synthesis;
+using System.Speech.Synthesis.TtsEngine;
+
+
+
+namespace vCon_1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+             
+            notifyIcon1.ShowBalloonTip(50);
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+
+            SpeechRecognizer sr = new SpeechRecognizer();
+            Choices commands = new Choices();
+            commands.Add(new string[] { "up", "down", "right", "left", "ok", "back", "tab","escape", "close" });
+
+            GrammarBuilder gbuilder = new GrammarBuilder();
+            gbuilder.Append(commands);
+
+            // Create the Grammar instance.
+            Grammar g = new Grammar(gbuilder);
+            sr.LoadGrammar(g);
+            g.Priority = 127;
+            sr.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sr_SpeechRecognized);
+
+        }
+        void sr_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string str = e.Result.Text;
+            if (str == "up")
+            {
+                System.Windows.Forms.SendKeys.Send("{UP}");
+            }
+
+            else if (str == "down")
+            {
+                System.Windows.Forms.SendKeys.Send("{DOWN}");
+            }
+
+            else if (str == "right")
+            {
+                System.Windows.Forms.SendKeys.Send("{RIGHT}");
+            }
+
+            else if (str == "left")
+            {
+                System.Windows.Forms.SendKeys.Send("{LEFT}");
+            }
+           
+
+            else if (str == "ok")
+            {
+                System.Windows.Forms.SendKeys.Send("{ENTER}");
+            }
+            else if (str == "back")
+            {
+                System.Windows.Forms.SendKeys.Send("{BACKSPACE}");
+            }
+            else if (str == "tab")
+            {
+                System.Windows.Forms.SendKeys.Send("{TAB}");
+            }
+            else if (str == "escape")
+            {
+                System.Windows.Forms.SendKeys.Send("{ESC}");
+            }
+            else if (str == "close")
+            {
+                System.Windows.Forms.SendKeys.Send("{^W}");
+            }
+           
+
+           
+
+
+            
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(50);
+        }
+
+       
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Name\tvCon 1.0\nVersion\tv1.0\nAuthor\tA.M. Masudul Haque\nEmail\tsaadixl@gmail.com","About vCon 1.0");
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+
+
+      
+    }
+}
